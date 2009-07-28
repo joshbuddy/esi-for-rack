2e007ce50af77d778e62c82cf15913a1d3fd8fc2
Joshua Hull <joshbuddy@gmail.com>
2009-07-27 21:31:21 -0400

    added 302 resolution

--- lib/esi_for_rack/lookup.rb
+++ lib/esi_for_rack/lookup.rb
@@ -7,17 +7,59 @@ class EsiForRack
     
     class Http
       
+      class RedirectFollower
+        class TooManyRedirects < StandardError; end
+
+        attr_accessor :url, :body, :redirect_limit, :response, :code
+
+        def initialize(url, limit=5)
+          @url, @redirect_limit = url, limit
+          logger.level = Logger::INFO
+        end
+
+        def logger
+          @logger ||= Logger.new(STDOUT)
+        end
+
+        def resolve
+          raise TooManyRedirects if redirect_limit < 0
+
+          self.response = Net::HTTP.get_response(URI.parse(url))
+
+          logger.info "redirect limit: #{redirect_limit}" 
+          logger.info "response code: #{response.code}" 
+          logger.debug "response body: #{response.body}" 
+
+          if response.kind_of?(Net::HTTPRedirection)      
+            self.url = redirect_url
+            self.redirect_limit -= 1
+
+            logger.info "redirect found, headed to #{url}" 
+            resolve
+          end
+
+          self.body = response.body
+          self.code = response.code
+          self
+        end
+
+        def redirect_url
+          if response['location'].nil?
+            response.body.match(/<a href=\"([^>]+)\">/i)[1]
+          else
+            response['location']
+          end
+        end
+      end
+
+
       def initialize(app, env)
         @app = app
         @env = env
       end
       
       def [](path)
-        return unless path[0,4] == 'http'
-        uri = URI(path)
-        res = Net::HTTP.start(uri.host, uri.port) {|http|
-          http.get(uri.request_uri)
-        }
+        res = RedirectFollower.new(path).resolve
         res.body if res.code == '200'
       end
       
